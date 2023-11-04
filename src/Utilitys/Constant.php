d71ed5c512b36604f724dbc60c3a2c9587bd4c82
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2023-11-04 12:54:51 +0700

    update url

--- src/Utilitys/Constant.php
+++ src/Utilitys/Constant.php
@@ -2,13 +2,11 @@
 
 namespace syahrulzzadie\SatuSehat\Utilitys;
 
-define('_urlHost_','https://api-satusehat.kemkes.go.id');
-
 class Constant
 {
-    public static $authUrl = _urlHost_."/oauth2/v1";
-    public static $baseUrl = _urlHost_."/fhir-r4/v1";
-    public static $consentUrl = _urlHost_."/consent/v1";
+    public static $authUrl = "https://api-satusehat.kemkes.go.id/oauth2/v1";
+    public static $baseUrl = "https://api-satusehat.kemkes.go.id/fhir-r4/v1";
+    public static $consentUrl = "https://api-satusehat.dto.kemkes.go.id/consent/v1";
     public static $clientId = "PG6mOnWoj8kexgZEuEw6DfROIjEfcEpIKxdpZwle5KnQzt2i";
     public static $clientSecret = "czAwICyCPcGvEPMtdK5ih0K4LCYT2pPY7H1TW7iuB9yWzGI2qyR3de5ORAPVgDRz";
     public static $organizationId = "100028165";
