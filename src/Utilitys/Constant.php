b564c7e1408fbd3905eae1b2c0bfb984b83c7db5
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2023-11-04 12:57:50 +0700

    Update Constant.php

--- src/Utilitys/Constant.php
+++ src/Utilitys/Constant.php
@@ -6,7 +6,7 @@ class Constant
 {
     public static $authUrl = "https://api-satusehat.kemkes.go.id/oauth2/v1";
     public static $baseUrl = "https://api-satusehat.kemkes.go.id/fhir-r4/v1";
-    public static $consentUrl = "https://api-satusehat.dto.kemkes.go.id/consent/v1";
+    public static $consentUrl = "https://api-satusehat.kemkes.go.id/consent/v1";
     public static $clientId = "PG6mOnWoj8kexgZEuEw6DfROIjEfcEpIKxdpZwle5KnQzt2i";
     public static $clientSecret = "czAwICyCPcGvEPMtdK5ih0K4LCYT2pPY7H1TW7iuB9yWzGI2qyR3de5ORAPVgDRz";
     public static $organizationId = "100028165";
