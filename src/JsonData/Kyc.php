342e04d3d9fb489102028d7b356d3abe0814cb09
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2023-12-19 21:13:40 +0700

    Update Kyc.php

--- src/JsonData/Kyc.php
+++ src/JsonData/Kyc.php
@@ -6,19 +6,29 @@ use syahrulzzadie\SatuSehat\Utilitys\Enviroment;
 
 class Kyc
 {
-    public static function formDataGenerateUrl($nik,$name)
+    private static function encriptGeneralUrl($nik,$name)
     {
         $publicKey = Enviroment::publicKey();
-        return [
+        $data = [
             'agent_name' => $name,
             'agent_nik' => $nik,
             'public_key' => $publicKey
         ];
+        $jsonData = json_encode($data);
+        return base64_encode($jsonData);
     }
 
-    public static function formDataChallengeCode($nik,$name)
+    public static function formDataGenerateUrl($nik,$name)
     {
-        return [
+        $data = "-----BEGIN ENCRYPTED MESSAGE-----\n";
+        $data .= self::encriptGeneralUrl($nik,$name);
+        $data .= "\n-----END ENCRYPTED MESSAGE-----";
+        return $data;
+    }
+
+    private static function encriptChallengeCode($nik,$name)
+    {
+        $data = [
             'metadata' => [
                 'method' => 'request_per_nik'
             ],
@@ -27,5 +37,15 @@ class Kyc
                 'name' => $name
             ]
         ];
+        $jsonData = json_encode($data);
+        return base64_encode($jsonData);
+    }
+
+    public static function formDataChallengeCode($nik,$name)
+    {
+        $data = "-----BEGIN ENCRYPTED MESSAGE-----\n";
+        $data .= self::encriptChallengeCode($nik,$name);
+        $data .= "\n-----END ENCRYPTED MESSAGE-----";
+        return $data;
     }
 }
