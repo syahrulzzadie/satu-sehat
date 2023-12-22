0070a94b439ba1169c972f6a0188092d4657d950
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2023-12-22 10:54:15 +0700

    update kyc

--- src/JsonData/Kyc.php
+++ src/JsonData/Kyc.php
@@ -2,44 +2,130 @@
 
 namespace syahrulzzadie\SatuSehat\JsonData;
 
+use phpseclib3\Crypt\AES;
+use phpseclib3\Crypt\PublicKeyLoader;
+use phpseclib3\Crypt\Random;
+use phpseclib3\Crypt\RSA;
+
 class Kyc
 {
-    private static function generate_rsa_key_pair()
+    private static function generateKey()
     {
-        $config = array(
-            "private_key_bits" => 2048,
-            "private_key_type" => OPENSSL_KEYTYPE_RSA,
-        );
-        $rsaKey = openssl_pkey_new($config);
-        openssl_pkey_export($rsaKey,$privateKey);
-        $publicKey = openssl_pkey_get_details($rsaKey)['key'];
-        return array($publicKey,$privateKey);
+        $config = [
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            'private_key_bits' => 2048,
+        ];
+        $keyPair = openssl_pkey_new($config);
+        $publicKey = openssl_pkey_get_details($keyPair)['key'];
+        openssl_pkey_export($keyPair, $privateKey);
+        return [
+            'publicKey' => $publicKey,
+            'privateKey' => $privateKey,
+        ];
     }
 
-    private static function generate_aes_key()
+    private static function importRsaKey($pem)
     {
-        return base64_encode(openssl_random_pseudo_bytes(32)); // 32 bytes untuk AES-256
+        $pemHeader = '-----BEGIN PUBLIC KEY-----';
+        $pemFooter = '-----END PUBLIC KEY-----';
+        $pemContents = substr($pem, strlen($pemHeader), strlen($pem) - strlen($pemFooter));
+        $binaryDerString = base64_decode($pemContents);
+        $tempFile = tempnam(sys_get_temp_dir(), 'rsa_key');
+        file_put_contents($tempFile, $binaryDerString);
+        $key = openssl_pkey_get_public('file://'.$tempFile);
+        openssl_pkey_get_details($key);
+        unlink($tempFile);
+        return $key;
     }
 
-    public static function encrypt_data_aes($publicKey,$data)
+    private static function generateSymmetricKey()
     {
-        $dataJson = json_encode($data);
-        $aesKey = self::generate_aes_key();
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length("aes-256-cbc"));
-        $encrypted = openssl_encrypt($dataJson,'aes-256-cbc',$publicKey,0,$iv);
-        openssl_public_encrypt($aesKey,$encrypted,$publicKey);
-        return '-----BEGIN ENCRYPTED MESSAGE-----\n'.$encrypted.'\n-----END ENCRYPTED MESSAGE-----';
+        $cryptoStrong = true;
+        $key = openssl_random_pseudo_bytes(32, $cryptoStrong);
+        if ($cryptoStrong !== true) {
+            return null;
+        }
+        return $key;
     }
 
-    public static function formDataGenerateUrl($nik,$name)
+    private static function generateRSAKeyPair()
     {
-        list($publicKey,$privateKey) = self::generate_rsa_key_pair();
+        $privateKeyConfig = [
+            'digest_alg' => 'sha256',
+            'private_key_bits' => 2048,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ];
+        $privateKeyResource = openssl_pkey_new($privateKeyConfig);
+        $privateKeyDetails = openssl_pkey_get_details($privateKeyResource);
+        $publicKey = $privateKeyDetails['key'];
+        $result = [
+            'privateKey' => $privateKeyResource,
+            'publicKey' => $publicKey,
+        ];
+        return $result;
+    }
+
+    private static function formatMessage($data)
+    {
+        $dataAsBase64 = chunk_split(base64_encode($data));
+        return "-----BEGIN ENCRYPTED MESSAGE-----\r\n{$dataAsBase64}-----END ENCRYPTED MESSAGE-----";
+    }
+
+    private static function aesEncrypt($data, $symmetricKey)
+    {
+        $ivLength = 12;
+        if (function_exists('random_bytes')) {
+            $iv = random_bytes($ivLength);
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            $iv = openssl_random_pseudo_bytes($ivLength);
+        } else {
+            $iv = '';
+            for ($i = 0; $i < $ivLength; $i++) {
+                $iv .= chr(mt_rand(0, 255));
+            }
+        }
+        $cipher = new AES('gcm');
+        $cipher->setKeyLength(256);
+        $cipher->setKey($symmetricKey);
+        $cipher->setNonce($iv);
+        $ciphertext = $cipher->encrypt($data);
+        $tag = $cipher->getTag();
+        $encryptedData = $iv.$ciphertext.$tag;
+        return $encryptedData;
+    }
+
+    private static function encryptMessage($message, $pubPEM)
+    {
+        $aesKey = self::generateSymmetricKey();
+        $serverKey = PublicKeyLoader::load($pubPEM);
+        $serverKey = $serverKey->withPadding(RSA::ENCRYPTION_OAEP);
+        $wrappedAesKey = $serverKey->encrypt($aesKey);
+        $encryptedMessage = self::aesEncrypt($message, $aesKey);
+        $payload = $wrappedAesKey.$encryptedMessage;
+        return self::formatMessage($payload);
+    }
+
+    public static function formDataGenerateUrl($agen_nik,$agen_name)
+    {
+        $keyPair = self::generateKey();
+        $publicKey = $keyPair['publicKey'];
+        $pubPEM = '-----BEGIN PUBLIC KEY-----
+        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxLwvebfOrPLIODIxAwFp
+        4Qhksdtn7bEby5OhkQNLTdClGAbTe2tOO5Tiib9pcdruKxTodo481iGXTHR5033I
+        A5X55PegFeoY95NH5Noj6UUhyTFfRuwnhtGJgv9buTeBa4pLgHakfebqzKXr0Lce
+        /Ff1MnmQAdJTlvpOdVWJggsb26fD3cXyxQsbgtQYntmek2qvex/gPM9Nqa5qYrXx
+        8KuGuqHIFQa5t7UUH8WcxlLVRHWOtEQ3+Y6TQr8sIpSVszfhpjh9+Cag1EgaMzk+
+        HhAxMtXZgpyHffGHmPJ9eXbBO008tUzrE88fcuJ5pMF0LATO6ayXTKgZVU0WO/4e
+        iQIDAQAB
+        -----END PUBLIC KEY-----';
         $data = [
-            'agent_name' => $name,
-            'agent_nik' => $nik,
+            'agent_name' => $agen_name,
+            'agent_nik' => $agen_nik,
             'public_key' => $publicKey
         ];
-        return self::encrypt_data_aes($publicKey,$data);
+        $jsonData = json_encode($data);
+        $encryptedPayload = self::encryptMessage($jsonData, $pubPEM);
+        return $encryptedPayload;
     }
 
     public static function formDataChallengeCode($nik,$name)
--- src/JsonResponse/Kyc.php
+++ src/JsonResponse/Kyc.php
@@ -2,10 +2,63 @@
 
 namespace syahrulzzadie\SatuSehat\JsonResponse;
 
+use phpseclib3\Crypt\AES;
+use phpseclib3\Crypt\PublicKeyLoader;
+use phpseclib3\Crypt\Random;
+use phpseclib3\Crypt\RSA;
+
 class Kyc
 {
+    private static function generateKey()
+    {
+        $config = [
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            'private_key_bits' => 2048,
+        ];
+        $keyPair = openssl_pkey_new($config);
+        $publicKey = openssl_pkey_get_details($keyPair)['key'];
+        openssl_pkey_export($keyPair, $privateKey);
+        return [
+            'publicKey' => $publicKey,
+            'privateKey' => $privateKey,
+        ];
+    }
+
+    private static function aesDecrypt($encryptedData, $symmetricKey)
+    {
+        $cipher = 'aes-256-gcm';
+        $ivlen = openssl_cipher_iv_length($cipher);
+        $tag_length = 16;
+        $iv = substr($encryptedData,0,$ivlen);
+        $tag = substr($encryptedData,-$tag_length);
+        $ciphertext = substr($encryptedData,$ivlen,-$tag_length);
+        $ciphertext_raw = openssl_decrypt($ciphertext, $cipher, $symmetricKey, OPENSSL_NO_PADDING, $iv, $tag);
+        return $ciphertext_raw;
+    }
+
+    private static function decryptMessage($message, $privateKey)
+    {
+        $beginTag = '-----BEGIN ENCRYPTED MESSAGE-----';
+        $endTag = '-----END ENCRYPTED MESSAGE-----';
+        $messageContents = substr(
+            $message,
+            strlen($beginTag) + 1,
+            strlen($message) - strlen($endTag) - strlen($beginTag) - 2
+        );
+        $binaryDerString = base64_decode($messageContents);
+        $wrappedKeyLength = 256;
+        $wrappedKey = substr($binaryDerString, 0, $wrappedKeyLength);
+        $encryptedMessage = substr($binaryDerString, $wrappedKeyLength);
+        $key = PublicKeyLoader::load($privateKey);
+        $aesKey = $key->decrypt($wrappedKey);
+        return self::aesDecrypt($encryptedMessage, $aesKey);
+    }
+
     public static function convertGenerateUrl($response) : array
     {
+        $keyPair = self::generateKey();
+        $privateKey = $keyPair['privateKey'];
+        $response = self::decryptMessage($response, $privateKey);
         $data = json_decode($response,true);
         $cekMetadata = isset($data['metadata']);
         if ($cekMetadata) {
